223329f7a69b7260b9ff2ead83dac4805ab604bf
Tadiwa Kadango <tadi@Tadiwas-MacBook-Pro.local>
2025-06-11 23:03:26 -0400

    updating footer

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
 	const { publication } = useAppContext();
 	const PUBLICATION_LOGO = publication.preferences.logo;
 	return (
-		<footer className="border-t bg-cwxnavy py-20 dark:border-neutral-800 ">
+		<footer className="border-t py-20 dark:border-neutral-800 ">
 			<Container className="px-5">
 				{PUBLICATION_LOGO ? (
 					<div className="mb-20 flex w-full flex-row justify-center">
@@ -27,22 +27,22 @@ export const Footer = () => {
 				<div className="grid w-full grid-cols-3 gap-5 md:grid-cols-6 lg:grid-cols-5">
 					<div className="col-span-1 grid grid-cols-4 gap-5 md:col-span-4 lg:col-span-3">
 						<div className="col-span-full md:col-span-2 lg:col-span-1">
-							<p className="text-white mb-2 font-semibold text-slate-600 dark:text-neutral-200">
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">
 								Stay in touch
 							</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<a href="https://discord.gg/cyberwoxacademy" className="text-white hover:underline">
+									<a href="https://discord.gg/cyberwoxacademy" className="hover:underline">
 										Contact us
 									</a>
 								</li>
 							</ul>
 						</div>
 						<div className="col-span-full md:col-span-2 lg:col-span-1">
-							<p className="text-white mb-2 font-semibold text-slate-600 dark:text-neutral-200">Resources</p>
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Resources</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<a href="https://courses.cyberwoxacademy.com" className="text-white hover:underline">
+									<a href="https://courses.cyberwoxacademy.com" className="hover:underline">
 										Courses
 									</a>
 								</li>
@@ -74,10 +74,10 @@ export const Footer = () => {
 							</ul>
 						</div> */}
 						<div className="col-span-1">
-							<p className="text-white mb-2 font-semibold text-slate-600 dark:text-neutral-200">Other links</p>
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Other links</p>
 							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
 								<li>
-									<a href="https://www.youtube.com/@cyberwoxacademy" className="text-white hover:underline">
+									<a href="https://www.youtube.com/@cyberwoxacademy" className="hover:underline">
 										Youtube
 									</a>
 								</li>
@@ -87,14 +87,14 @@ export const Footer = () => {
 									</a>
 								</li> */}
 								<li>
-									<a href="https://cyberwoxacademy.com/about-us" className="text-white hover:underline">
+									<a href="https://cyberwoxacademy.com/about-us" className="hover:underline">
 										About us
 									</a>
 								</li>
 							</ul>
 						</div>
 					</div>
-					<div className="text-white col-span-2 flex flex-col items-end gap-5 text-right text-slate-600 dark:text-neutral-300 md:text-left">
+					<div className="col-span-2 flex flex-col items-end gap-5 text-right text-slate-600 dark:text-neutral-300 md:text-left">
 						<SocialLinks />
 						<p>&copy; 2025 Cyberwox Academy </p>
 						{/* <p>
